1faf4d536228a9343faa1556386d504459938eff
Pranali <pranalibhandekar22@navgurukul.org>
2023-10-05 12:14:25 +0530

    Day-1 exercise done

---
+++ variable.js
@@ -0,0 +1,32 @@
+let firstName = 'pranali'
+let lastName
+// console.log(firstName,lastName)
+let abc=("")
+let aName=true
+console.log(firstName,lastName,abc,aName)
+
+// variables without assigning values
+let _var1
+let $1name
+let max_02
+let _Number
+let capital
+
+// declaring a variable multiple line....
+ let firstNAme="Pranali"
+ let lastNAme="Bhandekar"
+ let maritalstatus="unmarried"
+ const country="India"
+let age=19
+
+// in single line..
+let firstNAMe="Pranali",  lastNAME="Bhandekar" ,maritalStatus="unmarried", counTry="India",  aGe=19
+
+// declaring two variable
+ let myAge="i am 19 years old"
+ let yourAge="you are 25 years old"
+console.log(myAge)
+console.log(yourAge)
+
+
+
