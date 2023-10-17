ee6e4d7a19294017e7eeb7b53fc5844496b423ec
Pranali3232 <144037864+Pranali3232@users.noreply.github.com>
2023-10-17 12:34:24 +0530

    Add files via upload

---
+++ Day3/Exercise level2.js
@@ -0,0 +1,87 @@
+
+// let myAge=250
+// let yourAge=25
+// console.log('i am ' +(250-25)+' years older than you.')
+const prompt = require("prompt-sync")()
+
+// Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+// let base=prompt("enter the base")
+// let height=prompt("enter the height")
+// base=parseInt(base)
+// height=parseInt(height)
+// console.log("area"+ 0.5*base*height)
+
+// rite a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
+// let side1=prompt("entre the length of side1")
+// let side2=prompt("entre the length of side2")
+// let side3=prompt("entre the length of side3")
+// side1=parseInt(side1)
+// side2=parseInt(side2)
+// side3=parseInt(side3)
+// let perimeter= side1 + side2 + side3;
+// console.log("The perimeter of triangle is" + perimeter)
+
+// Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+// let length=("enter the length")
+// let width=("enter the width")
+// length=parseInt(length)
+// width=parseInt(width)
+// perimeter=2 *(length+width)
+// console.log(perimeter)//left....
+
+//Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+// let radius=parseInt(prompt("enter the radius"))
+// const pi=3.14
+// area=pi*radius*radius
+// circumference=2*pi*radius
+// console.log(area)
+// console.log(circumference)
+
+//Writ a script that prompt a user to enter hours and rate per hour. Calculate pay of the person?=parseInt(prompt("enter the hours"))
+// let hrs=parseInt(prompt("enter the hrs"))
+// let rphrs=parseInt(prompt("enter the rate per hour"))
+// earning=(hrs*rphrs)
+// console.log(earning)
+
+//If the length of your name is greater than 7 say, your name is long else say your name is short.
+// let name=parseInt(prompt("enter the name"))
+// let length=name.length
+// console.log(length>7) ? "your name is long":"your name is short"
+
+//Compare your first name length and your family name length and you should get this output.
+// let firstname=parseInt(prompt("enter the firstname")) 
+// let lastname=parseInt(prompt("enter yhe lastname"))
+// firstname=firstname.length
+// lastname=lastname.length
+// console.log(firstname>lastname) ? "first name is longer than last name" : "lastname is longer than first name"
+//......have to look in this que
+
+//Write a script that prompt the user to enter number of years. Calculate the number of seconds a person can live. Assume some one lives just hundred years
+// let years=parseInt(prompt("enter the year"))
+
+
+
+// a human readable time format using the Date time object
+// const now= new Date()
+// const year = now.getFullYear() 
+// const month = now.getMonth() + 1 
+// const date = now.getDate() 
+// const hours = now.getHours() 
+// const minutes = now.getMinutes()
+// console.log(`${year}/${month}/${date} ${hours}:${minutes}`)//YYYY-MM-DD HH:mm
+// console.log(`${date}/${month}/${year} ${hours}:${minutes}`)//DD-MM-YYYY HH:mm
+// console.log(`${date}/${year}/${month} ${hours}:${minutes}`)//DD/MM/YYYY HH:mm
+
+
+//EXERCISE3...
+// let now=new Date()
+// let year=now.getFullYear()
+// let month=now.getMonth()+1
+// let date=now.getDate()
+// let hours=now.getHours()
+// let minute=now.getMinutes()
+// month<10 ? month='0'+month:month
+// date<10 ? date='0'+date:date
+// hours<10 ? hours='0'+hours:hours
+// minute<10 ? minute='0'+minute:minute
+// console.log(`${year}-${month}-${date}-${hours}:${minute}`)
