a25aff6f3d841184cd1393a364182327f9c8d51c
Pranali3232 <144037864+Pranali3232@users.noreply.github.com>
2023-10-17 12:34:57 +0530

    Add files via upload

---
+++ Day4/Exercise level1.js
@@ -0,0 +1,42 @@
+const prompt = require("prompt-sync")()
+
+//user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+// let age=parseInt(prompt("enter the age"))
+// let a=18-age
+// if(age>18){
+//     console.log("You are old enough to drive")
+// }
+// else{
+//     console.log("You are left with" + a + "years to drive")
+// }
+
+//Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+// let yourage=parseInt(prompt("enter your age"))
+// if()
+
+
+
+//If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+//using if else
+// let a=4
+// let b=3
+// if(a>b){
+//     console.log(`${a} is greater than ${b}`)
+// }
+// else{
+//     console.log(`${b} is greater than ${a}`)
+// }
+//using ternary operator
+// console.log(a>b) ? "a is greater than b" : "b is greater than a"
+
+//Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
+// let num=parseInt(prompt("enter the number"))
+// if(num%2==0){
+//     console.log(`${num}is an even number`)
+// }
+// else{
+//     console.log(`${num}is an odd number`)
+// }
+
+
+
---
+++ Day4/Exercise level3.js
@@ -0,0 +1,80 @@
+const prompt = require("prompt-sync")()
+
+//Write a program which tells the number of days in a month.
+// let month= prompt("Enter the month:")
+// if(month=="january"){
+//     console.log("January has 31 days")
+// }
+// else if(month=="february"){
+//     console.log("february has 28 days")
+// }
+// else if(month=="march"){
+//     console.log("march has 31 days")
+// }
+// else if(month=="april"){
+//     console.log("april has 30 days")
+// }
+// else if(month=="may"){
+//     console.log("may has 31 days")
+// }
+// else if(month=="june"){
+//     console.log("june has 30 days")
+// }
+// else if(month=="july"){
+//     console.log("july has 31 days")
+// }
+// else if(month=="augest"){
+//     console.log("augest has 31 days")
+// }
+// else if(month=="september"){
+//     console.log("september has 30 days")
+// }
+// else if(month=="october"){
+//     console.log("october has 31 days")
+// }
+// else if(month=="november"){
+//     console.log("november has 30 days")
+// }
+// else if(month=="december"){
+//     console.log("december has 31 days")
+// }  
+
+//Write a program which tells the number of days in a month, now consider leap year.
+// let month= prompt("Enter the month:")
+// if(month=="january"){
+//     console.log("January has 31 days")
+// }
+// else if(month=="february"){
+//     console.log("february has 29 days")
+// }
+// else if(month=="march"){
+//     console.log("march has 31 days")
+// }
+// else if(month=="april"){
+//     console.log("april has 30 days")
+// }
+// else if(month=="may"){
+//     console.log("may has 31 days")
+// }
+// else if(month=="june"){
+//     console.log("june has 30 days")
+// }
+// else if(month=="july"){
+//     console.log("july has 31 days")
+// }
+// else if(month=="augest"){
+//     console.log("augest has 31 days")
+// }
+// else if(month=="september"){
+//     console.log("september has 30 days")
+// }
+// else if(month=="october"){
+//     console.log("october has 31 days")
+// }
+// else if(month=="november"){
+//     console.log("november has 30 days")
+// }
+// else if(month=="december"){
+//     console.log("december has 31 days")
+// }  
+
